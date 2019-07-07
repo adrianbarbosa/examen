05b2d5260f9a2c17eea63a87fcf7571a156d131a
Adrian Barbosa <abarbosaluna@gmail.com>
2019-07-07 10:19:39 -0500

    Created Landing, Home and Album components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import logo from "./logo.svg";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Home from "./components/home/Home";
+import Album from "./components/album/Album";
+import Footer from "./components/layout/Footer";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        {/* <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            </Switch> */}
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/album" component={Album} />
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layout/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Footer extends Component {
+  render() {
+    return <footer className="footer fixed-bottom p-3" />;
+  }
+}
+
+export default Footer;
