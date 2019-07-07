be27c43266e04dd37fa7e1bf072f423aec5b0914
Adrian Barbosa <abarbosaluna@gmail.com>
2019-07-07 16:57:32 -0500

    Posts showing on users

--- src/components/layout/Login.js
+++ src/components/layout/Login.js
@@ -32,12 +32,7 @@ class Login extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    const userData = {
-      email: this.state.email,
-      password: this.state.password
-    };
-
-    this.props.loginUser(userData);
+    this.props.history.push("/home");
   }
 
   onChange(e) {
